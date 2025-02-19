d2db61c10c417ba41d5caa9864ea47c5a7a892d5
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2025-02-19 12:06:38 +0530

    folder structure change and route settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import Banner from "./Components/Banner";
-import FeaturesSection from "./Components/FeaturesSection"; // Import FeaturesSection
-import ProductGrid from "./Components/ProductGrid";
-import WhyChoose from "./Components/WhyChoose";
-import StatsSection from "./Components/StatsSection";
-import Testimonials from "./Components/Testimonials";
-import Footer from "./Components/Footer";
-function App() {
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Home from './Pages/home/Home';
+import Layout from './Components/common/Layout';
+
+const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Banner />
-      <div >
-        <FeaturesSection />
-      </div>
-      <div className="bg-gray-100 min-h-screen">
-      <ProductGrid />
-    </div>
-    <div>
-      <WhyChoose/>
-    </div>
-    <div>
-      <StatsSection/>
-    </div>
-    <div>
-      <Testimonials/>
-    </div>
-    <div className="mt-[100px]">
-        <Footer />
-      </div>
-    </div>
+    <Router> 
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
